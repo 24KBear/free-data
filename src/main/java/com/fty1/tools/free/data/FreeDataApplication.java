a1fa5b28910f54fd72185bb255e25b7ea339b045
daisy <daisy@daisydeMacBook-Pro.local>
2022-09-17 18:46:55 +0800

    Initial commit

---
+++ src/main/java/com/fty1/tools/free/data/FreeDataApplication.java
@@ -0,0 +1,13 @@
+package com.fty1.tools.free.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreeDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fty1/tools/free/data/FreeDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fty1.tools.free.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
